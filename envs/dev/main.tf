a4d2367ce8ea1da88c9efed0120c524f713287f7
El-shado <chadi.kawaf@gmail.com>
2025-09-18 10:38:08 +0200

    feat(dev): add ssh_ingress_cidr variable and wire it to security module

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -3,10 +3,25 @@ module "vpc" {
 
   vpc_cidr           = "10.0.0.0/16"
   public_subnet_cidr = "10.0.1.0/24"
+
   tags = {
-    project = "terraform-mini"
+    project = "terraform-ansible-aws"
     env     = "dev"
-    owner   = "<chadi.kawaf@gmail.com>"
+    owner   = "El-shado"
+    cost    = "free-tier"
+  }
+}
+
+module "security" {
+  source = "../../modules/security"
+
+  vpc_id           = module.vpc.vpc_id
+  ssh_ingress_cidr = var.ssh_ingress_cidr
+
+  tags = {
+    project = "terraform-ansible-aws"
+    env     = "dev"
+    owner   = "El-shado"
     cost    = "free-tier"
   }
 }
---
+++ envs/dev/variables.tf
@@ -0,0 +1,4 @@
+variable "ssh_ingress_cidr" {
+  description = "My public IP as /32 for SSH"
+  type        = string
+}
